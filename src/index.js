3a60d420d40cb11fa56fb73343f4511b068168ad
刘浩东hollton (170776) <170776@nd.com>
2021-11-02 12:42:45 +0800

    修复滚动数据height

--- src/index.js
+++ src/index.js
@@ -197,16 +197,8 @@ var LuckyPicker = function (_config = {}, _option = {}) {
     }
 
     function isPC() {
-        var userAgentInfo = navigator.userAgent;
-        var Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
-        var flag = true;
-        for (var i = 0; i < Agents.length; i++) {
-            if (userAgentInfo.indexOf(Agents[i]) > 0) {
-                flag = false;
-                break;
-            }
-        }
-        return flag;
+        var ua = navigator.userAgent;
+        return /Windows|Mac\ OS\ X\ /ig.test(ua);
     }
 
     function destroy() {
